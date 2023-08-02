547ba6a7991cd0d113e0681de417f5c6d920a47b
seoyeoning <minalunpark@naver.com>
2023-08-02 11:48:25 +0900

    first

---
+++ src/main/java/com/example/github_actions_test/controller.java
@@ -0,0 +1,15 @@
+package com.example.github_actions_test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "hello";
+    }
+}
