08c30d5870a92592e7892a1444570a8d9f4fd533
seoyeoning <minalunpark@naver.com>
2023-08-07 14:41:34 +0900

    db 연결

---
+++ src/main/java/com/example/github_actions_test/Dao.java
@@ -0,0 +1,30 @@
+package com.example.github_actions_test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class Dao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void test(int id, String name) {
+
+        String query = "insert into test (id, name)\n" + "VALUES (?, ?)";
+        Object[] param = new Object[]{id, name};
+
+        this.jdbcTemplate.update(query,param);
+
+    }
+
+
+
+}
--- src/main/java/com/example/github_actions_test/controller.java
+++ src/main/java/com/example/github_actions_test/controller.java
@@ -1,14 +1,21 @@
 package com.example.github_actions_test;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
 
+    private final Dao dao;
+
+    public controller(Dao dao) {
+        this.dao = dao;
+    }
+
     @GetMapping("/")
     public String home() {
+
+        dao.test(3, "c");
 
         return "배포 완료!!";
     }
